7088231ab342a977e9b03c8e363f567f75c60611
swenht <swenht@163.com>
2019-07-11 14:39:13 +0800

    springcloud myself

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.cfgbeans;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
+@Configuration
+public class ConfigBean {
+	@Bean
+	@LoadBalanced
+	public RestTemplate geRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public IRule myRule() {
+		// return new RoundRobinRule();s
+		return new RandomRule();
+	}
+
+}
